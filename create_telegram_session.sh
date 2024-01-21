0a80a9a8d9025b966f882d282a5c709e65f0205f
Aflt <mr.ali1377@yahoo.com>
2024-01-21 18:10:56 +0330

    added network host for proxies

--- create_telegram_session.sh
+++ create_telegram_session.sh
@@ -1,1 +1,1 @@
-docker run --rm -it  -v "$PWD":/app -w /app yt_insta_downloader python3 create_telegram_session.py --config config.yaml
+docker run --rm -it --network host -v "$PWD":/app -w /app yt_insta_downloader python3 create_telegram_session.py --config config.yaml
--- get_user_ids.sh
+++ get_user_ids.sh
@@ -1,1 +1,1 @@
-docker run --rm -it  -v "$PWD":/app -w /app yt_insta_downloader python3 get_user_ids.py --config config.yaml
+docker run --rm -it --network host -v "$PWD":/app -w /app yt_insta_downloader python3 get_user_ids.py --config config.yaml
